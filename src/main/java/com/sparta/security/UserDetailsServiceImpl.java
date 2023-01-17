3d78a08c8f13eeb30b01bfc35c2308d9e930eac2
SongeonLee <qkfka1040@icloud.com>
2023-01-17 16:13:21 +0900

    Feat: 스프링시큐리티 추가
    UserDetailsImpl, UserDetailsServiceImpl 추가

--- src/main/java/com/sparta/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/security/UserDetailsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.sparta.security;
 
+import com.sparta.domain.User;
+import com.sparta.exception.ErrorCode;
+import com.sparta.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,9 +13,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(ErrorCode.NOT_FOUND_USER.getMessage());
+        return new UserDetailsImpl(user, user.getUsername());
     }
 
 }
