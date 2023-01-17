8422b5a7a1bcb3573bf690cbaece246f70f74a0c
SongeonLee <qkfka1040@icloud.com>
2023-01-17 15:50:56 +0900

    Feat: 스프링시큐리티
    securityConfig, 401, 403 예외처리

---
+++ src/main/java/com/sparta/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.sparta.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.dto.ResponseStatusDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private static final ResponseStatusDto statusDto = new ResponseStatusDto(HttpStatus.UNAUTHORIZED.toString(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        try (OutputStream os = response.getOutputStream()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(os, statusDto);
+            os.flush();
+        }
+    }
+}
