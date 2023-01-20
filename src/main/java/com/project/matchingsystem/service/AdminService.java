209df19c2a191217486f3e62541d5b7c0a2cebfd
SangHwan <skyi0909@naver.com>
2023-01-20 22:56:46 +0900

    fix - errocode 수정

--- src/main/java/com/project/matchingsystem/service/AdminService.java
+++ src/main/java/com/project/matchingsystem/service/AdminService.java
@@ -54,21 +54,21 @@ public class AdminService {
 
         // 요청상태가 drop확인 - DROP되면 다시는 처리 더이상 안됨
         if (sellerManagement.getRequestStatus() == SellerManagementStatusEnum.DROP) {
-            return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "거부");
+            throw new IllegalArgumentException(ErrorCode.AREADY_SELLERMAGEMENT_STATUS_DROP.getMessage());
         }
         // 요청상태가 COMPLETE확인 - COMPLETE되면 다시는 처리 더이상 안됨
         if (sellerManagement.getRequestStatus() == SellerManagementStatusEnum.COMPLETE) {
-            return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "이미 판매자 승인된 요청입니다.");
+            throw new IllegalArgumentException(ErrorCode.AREADY_SELLERMAGEMENT_STATUS_COMPLETE.getMessage());
         }
         // 요청상태가 REJECT확인 - COMPLETE되면 다시는 처리 더이상 안됨
         if (sellerManagement.getRequestStatus() == SellerManagementStatusEnum.REJECT) {
-            return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "취소 처리된 요청입니다.");
+            throw new IllegalArgumentException(ErrorCode.AREADY_SELLERMAGEMENT_STATUS_REJECT.getMessage());
         }
 
         user.permitRoleUser(); //유저 권한 판매자로 변경
         sellerManagement.completeRequestStatus(); //권한 요청 데이터의 요청상태 complete로 전환
 
-        return new ResponseStatusDto(HttpStatus.OK.toString(), "판매자 권한 승인 완료");
+        return new ResponseStatusDto(HttpStatus.OK.toString(), "판매자 권한 승인 완료했습니다.");
     }
 
     @Transactional
@@ -82,15 +82,15 @@ public class AdminService {
         );
         // 요청상태가 drop확인 - DROP되면 다시는 처리 더이상 안됨
         if (sellerManagement.getRequestStatus() == SellerManagementStatusEnum.DROP) {
-            return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "거부");
+            throw new IllegalArgumentException(ErrorCode.AREADY_SELLERMAGEMENT_STATUS_DROP.getMessage());
         }
         //요청상태 Complete 시 더이상 처리 안함
         if (sellerManagement.getRequestStatus() == SellerManagementStatusEnum.COMPLETE) {
             user.dropRoleUser(); //유저 권한 user로 변경
             sellerManagement.dropRequestStatus(); //권한 요청 데이터의 요청상태 drop 전환
-            return new ResponseStatusDto(HttpStatus.OK.toString(), "판매자 권한 거절처리 완료");
+            return new ResponseStatusDto(HttpStatus.OK.toString(), "판매자 권한 회수 처리 완료했습니다.");
         }
-        return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "거절처리 할 수 없습니다.");
+        return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "판매자 권한 회수 처리 할 수 없습니다.");
     }
 
     @Transactional
@@ -106,17 +106,17 @@ public class AdminService {
 
         // 요청 상태가 WAIT확인 - DROP되면 다시는 안되는거 알려주기 wait을 cancel로
         if (sellerManagement.getRequestStatus() != SellerManagementStatusEnum.WAIT) {
-            return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "이미 대기상태의 판매자가 아닙니다. 대기 상태에만 적용 가능합니다.");
+            throw new IllegalArgumentException(ErrorCode.NOT_SELLERMAGEMENT_STATUS_WAIT.getMessage());
         }
 
         //요청 상태 Complete 시 더이상 처리 안함
         if (sellerManagement.getRequestStatus() == SellerManagementStatusEnum.COMPLETE) {
-            return new ResponseStatusDto(HttpStatus.BAD_REQUEST.toString(), "이미 판매자 승인 된 요청입니다.");
+            throw new IllegalArgumentException(ErrorCode.AREADY_SELLERMAGEMENT_STATUS_COMPLETE.getMessage());
         }
 
         user.dropRoleUser(); //유저 권한 user로 변경
         sellerManagement.rejectRequestStatus(); //권한 요청 데이터의 요청상태 reject로 전환
-        return new ResponseStatusDto(HttpStatus.OK.toString(), "판매자 권한 승인 요청 취소처리 완료");
+        return new ResponseStatusDto(HttpStatus.OK.toString(), "판매자 권한 승인 취소 처리 완료했습니다.");
     }
 
 }
