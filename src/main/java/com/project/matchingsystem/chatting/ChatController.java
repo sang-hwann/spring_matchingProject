8c644a68575b45e586bf43d99213bee75a215662
SongeonLee <qkfka1040@icloud.com>
2023-01-25 10:49:31 +0900

    COMMENT: 불필요한 주석 삭제

--- src/main/java/com/project/matchingsystem/chatting/ChatController.java
+++ src/main/java/com/project/matchingsystem/chatting/ChatController.java
@@ -9,21 +9,8 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/chat")  // 여기는 컨피그에서 막아둬야함.
+@RequestMapping("/chat")
 public class ChatController {
-
-    /*
-        현재 상태
-        1. 권한을 가진 사람만 채팅룸 개설 가능, 개설시 채팅할 셀러의 이름을 명시해야함.  -> config 에서 유저만 가능하게 변경해야함. 현재는 셀러도 가능
-        2. 메시지 타입을 통해 유저와 셀러 구분한다.
-
-        추가해야 할 것
-        1. 유저는 자신의 채팅방 목록을 조회할 수 있어야 한다.
-
-        +++
-        채팅방 삭제는 어떻게 하는가?
-     */
-
 
     private final ChatService chatService;
 
